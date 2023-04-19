fa43f2183533584886885be8792de33b6d1cf7bd
93Jefferson <jeffergs_@hotmail.com>
2023-04-19 10:55:08 -0500

    Primieng-Table-TabMenu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table'; 
+import { PanelModule } from "primeng/panel";
+import { TabMenuModule } from "primeng/tabmenu";
+
 import { AppComponent } from './app.component';
 import { InicioComponent } from './pages/inicio/inicio.component';
 import { LoadingComponent } from './component/loading/loading.component';
+
 import { AppRoutingModule } from './app.routing.module';
+
 import { PaginaTablaComponent } from './pages/pagina-tabla/pagina-tabla.component';
+import { HttpClientModule } from '@angular/common/http';
+
+
+
 
 
 @NgModule({
@@ -21,6 +32,10 @@ import { PaginaTablaComponent } from './pages/pagina-tabla/pagina-tabla.componen
     AppRoutingModule,
     BrowserAnimationsModule,
     ButtonModule,
+    HttpClientModule,
+    TableModule,
+    PanelModule,
+    TabMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/pagina-tabla/pagina-tabla.component.ts
+++ src/app/pages/pagina-tabla/pagina-tabla.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+import { IDataEmpleado, iEmpleados } from 'src/app/interfaces/empleadosInterfaces';
+import { EmpleadoService } from 'src/app/services/empleado.service';
 
 @Component({
   selector: 'app-pagina-tabla',
@@ -7,8 +10,69 @@ import { Router } from '@angular/router';
   styleUrls: ['./pagina-tabla.component.css']
 })
 export class PaginaTablaComponent {
-  constructor(private ruta: Router){
+  listEmpleado: IDataEmpleado[] = [];
+  columnTabla: any;
+  listMenu: MenuItem[] = [];
+  activeItem!: MenuItem;
+  constructor(private ruta: Router,    
+    private empleadoService: EmpleadoService){
     
+  }
+
+  ngOnInit(): void {
+    this.iniColumnTabla();
+    this.inicioMenu();
+    console.log('Hola estoy aqui desde ngOninit');
+    // this.empleadoService.getAllEmployee().subscribe(
+    //   (res) => {
+    //     console.log(res);
+    //     this.listEmpleado = res.data;
+    //   }, (error) => {
+    //     console.log(error);
+    //   }
+    // );
+
+    this.empleadoService.getAllEmployee().subscribe({
+      next: (datos) => {
+        console.log(datos);
+        this.listEmpleado = datos.data;
+      },
+      error: (err)=>{
+        console.log(err);
+      }
+    });
+
+  }
+
+  iniColumnTabla(){
+    this.columnTabla=[
+      {
+        field:'id', header:'ID'
+      },
+      {
+        field:'name', header:'Nombre Empleado'
+      },
+      {
+        field:'salary', header:'Salario Empleado'
+      },
+      {
+        field:'edad', header:'EDAD'
+      },  
+  ];
+  }
+
+  inicioMenu(){
+    this.listMenu=[
+      {
+        label:'Cliente' 
+      },
+      {
+        label:'Empresa' 
+      }
+    ];
+
+    this.activeItem = this.listMenu[0];
+
   }
 
   regresarInicio(){
